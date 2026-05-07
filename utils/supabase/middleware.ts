b7c3e030eed7c89faa9580f0dda39ebbfe1cac16
Evan Wright <evanwright2510@Evans-MacBook-Pro.local>
2026-05-07 19:53:48 -0400

    fix supabase middleware

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -15,10 +15,17 @@ export async function updateSession(request: NextRequest) {
           return request.cookies.getAll();
         },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => {
-            request.cookies.set(name, value);
-            response.cookies.set(name, value, options);
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value)
+          );
+
+          response = NextResponse.next({
+            request,
           });
+
+          cookiesToSet.forEach(({ name, value, options }) =>
+            response.cookies.set(name, value, options)
+          );
         },
       },
     }
